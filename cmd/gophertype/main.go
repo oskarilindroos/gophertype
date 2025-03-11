b57b6b58adc122573daa2d80ba594b703b6075fe
Oskari Lindroos <oskarilindroos00@gmail.com>
2025-03-11 16:42:04 +0200

    fixed container styling

--- cmd/gophertype/main.go
+++ cmd/gophertype/main.go
@@ -8,12 +8,8 @@ import (
 )
 
 var containerStyle = lipgloss.NewStyle().
-	Width(50).
-	Height(10).
-	Padding(4).
-	Align(lipgloss.Center, lipgloss.Center).
-	BorderStyle(lipgloss.RoundedBorder()).
-	BorderForeground(lipgloss.Color("1"))
+	Padding(2).
+	BorderStyle(lipgloss.RoundedBorder())
 
 type model struct {
 	width  int
@@ -26,21 +22,27 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
 	case tea.KeyMsg:
 		if s := msg.String(); s == "ctrl+c" || s == "q" || s == "esc" {
 			return m, tea.Quit
 		}
+
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
 		return m, nil
 	}
+
 	return m, nil
 }
 
 func (m model) View() string {
 	var s string
-	s += containerStyle.Render(fmt.Sprintf("way where second group more who group end house even question miss new own four up night number still make word any way where second group more who group end house even question miss new own four up night number still make word any"))
+
+	s += containerStyle.
+		Width(int(float64(m.width) * 0.5)).
+		Render(fmt.Sprintf("60\nway where second group more who group end house even question miss new own four up night number still make word any way where second group more who group end house even question miss new own four up night number still make word any"))
 
 	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, s)
 }
