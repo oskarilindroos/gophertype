74b845a374b7d720b56e0be07d668fb2b5139739
Oskari Lindroos <oskarilindroos00@gmail.com>
2025-03-27 15:13:14 +0200

    added user input and cursor

--- cmd/gophertype/main.go
+++ cmd/gophertype/main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"fmt"
+	"gophertype/internal/words"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"os"
 )
 
-var containerStyle = lipgloss.NewStyle().
-	Padding(2).
-	BorderStyle(lipgloss.RoundedBorder())
+var (
+	containerStyle = lipgloss.NewStyle().
+			Padding(2).
+			BorderStyle(lipgloss.RoundedBorder())
+
+	cursorStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("yellow")).
+			Background(lipgloss.Color("yellow")).
+			Underline(true)
+
+	correctWordStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("green"))
+
+	incorrectWordStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("red"))
+)
 
 type model struct {
-	width  int
-	height int
+	width        int
+	height       int
+	userInput    string
+	wordsManager *words.WordManager
+	cursor       int
+	correct      int
+	errors       int
+	wpm          int
+}
+
+func initialModel() model {
+	wordsManager := words.NewWordManager()
+
+	return model{
+		wordsManager: wordsManager,
+		cursor:       0,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -22,12 +52,21 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
-		if s := msg.String(); s == "ctrl+c" || s == "q" || s == "esc" {
-			return m, tea.Quit
-		}
+		switch msg.String() {
 
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+		case "backspace":
+			if m.cursor > 0 {
+				m.cursor--
+				m.userInput = m.userInput[:m.cursor]
+			}
+		default:
+			m.cursor++
+			m.userInput += msg.String()
+		}
+	// Update window width and height on resize
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
@@ -38,17 +77,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	var s string
+	var output string
 
-	s += containerStyle.
-		Width(int(float64(m.width) * 0.5)).
-		Render(fmt.Sprintf("60\nway where second group more who group end house even question miss new own four up night number still make word any way where second group more who group end house even question miss new own four up night number still make word any"))
+	// userInputWithCursor := m.userInput[:m.cursor] + cursorChar + m.userInput[m.cursor:]
+	cursorChar := cursorStyle.Render(" ")
+	userInputWithCursor := m.userInput[:m.cursor] + cursorChar + m.userInput[m.cursor:]
 
-	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, s)
+	output += containerStyle.
+		Width(int(float64(m.width) * 0.5)). // Takes up half the width
+		Render(fmt.Sprintf("%s", userInputWithCursor))
+
+	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, output)
 }
 
 func main() {
-	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
